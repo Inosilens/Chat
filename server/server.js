11f3b4197d84c7b28a1269917d609f2df9a6fa07
inosilens <pashin.andrey94@gmail.com>
2022-01-29 21:04:14 +0300

    revert again

--- server/server.js
+++ server/server.js
@@ -1,13 +1,10 @@
 const {GraphQLServer, PubSub} = require('graphql-yoga');
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const cors = require('cors');
-var { buildSchema } = require('graphql');
 
 
 const messages = []
-const schema = buildSchema(`
-   type Message {
+
+const typeDefs = `
+    type Message {
         id: ID!
         user : String!
         content : String!
@@ -20,8 +17,7 @@ const schema = buildSchema(`
     }
     type Subscription {
         messages : [Message!]
-    }
-`);
+    }`;
 
 const subscribers = []
 
@@ -56,27 +52,7 @@ const resolvers = {
         },
     },
 };
-const port = process.env.PORT || 3001
+
 const pubsub = new PubSub();
 const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 server.start(({port}) => console.log(`server start at port ${port}`))
-const app = express();
-app.use(cors());
-
-app.use(
-    '/graphql',
-    graphqlHTTP({
-        schema,
-        graphiql: true
-    })
-);
-
-app.use(express.static('public'));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
