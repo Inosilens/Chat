504ee9756037d63c652497e6c06e0f4c5e94d382
inosilens <pashin.andrey94@gmail.com>
2022-01-29 16:18:00 +0300

    Added data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,43 @@
-import React, {FC, useEffect} from 'react';
+import React from 'react';
+import {ApolloClient, ApolloProvider, gql, InMemoryCache, useQuery} from "@apollo/client";
+import List from "./component/list/List";
 
-interface IProps {
-    id: number,
-    name: string
-}
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache()
+});
 
 
-export const App: FC = ({}: IProps) => {
-    useEffect(() => {
+const GET_MESSAGE = gql`
+    query { 
+        messages {
+        id,
+        content,
+        user
+        }
+    }
+`
 
-    }, [])
-    return (
-        <div>
-            NOW WORK
-        </div>
-    );
+const App = () => {
+    const {data} = useQuery(GET_MESSAGE)
+    if (data) {
+        return (
+            <div>
+                <List messages={data.messages}/>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                Пустота
+            </div>
+        )
+    }
+
 };
 
+export default () =>
+    (<ApolloProvider client={client}>
+            <App/>
+        </ApolloProvider>
+    )
